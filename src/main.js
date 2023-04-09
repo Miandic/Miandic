83ecb21df227829849516986af4576e2fda0a29b
Miandic <misha.andreev.novo@gmail.com>
2023-04-09 10:49:10 +0300

    mistakes fixed

--- src/main.js
+++ src/main.js
@@ -39,8 +39,8 @@ const Write = (text, id) => {
 
 const TimeColor = (minutes) => {
     let c1 = ColorSteps.getColorSteps('#2b2924', '#866f4a', 180)
-    let c2 = ColorSteps.getColorSteps('#866f4a', '#e29939', 180)
-    let c3 = ColorSteps.getColorSteps('#e29939', '#ffe225', 360)
+    let c2 = ColorSteps.getColorSteps('#866f4a', '#a7722c', 180)
+    let c3 = ColorSteps.getColorSteps('#a7722c', '#e29939', 360)
     let mastercolor = []
     for (let i = 0; i < c1.length; i++) { mastercolor[i] = c1[i] }
     for (let i = 0; i < c2.length; i++) { mastercolor[i + 180] = c2[i] }
